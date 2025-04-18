ec682a2e8dd3b306404581e52fd7b85e92fd6bb2
Gurmesa <gurmesakedir96@gmail.com>
2025-04-19 01:07:39 +0300

    part-3 moies menu

--- react-mosh/src/App.jsx
+++ react-mosh/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Counter from './components/counter';
+import Movies from './components/Movies';
 
 function App() {
 
   return (
     <>
       <Counter />
+      <Movies />
     </>
   )
 }
