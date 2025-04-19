4a65143bb337ab6c65f0362419adc044d166d760
Gurmesa <gurmesakedir96@gmail.com>
2025-04-19 10:50:37 +0300

    part-5 styling the table add Delete button and its functionality

--- react-mosh/src/components/Movies.jsx
+++ react-mosh/src/components/Movies.jsx
@@ -6,6 +6,11 @@ class Movies extends Component {
     movies: getMovies()
      // Initialize movies from fakeMovies.js
   };
+
+  handleDelete = (movie) => {
+    const mov = this.state.movies.filter(m => m._id !== movie._id);
+    this.setState({movies:mov});
+  }
   render() { 
     const { movies } = this.state; // Destructure movies from state
     console.log(this.state.movies);
@@ -18,19 +23,23 @@ class Movies extends Component {
         <table className="table">
           <thead>
             <tr className='p-10px'>
-              <th className="px-5 py-2 mx-5 text-start">Title</th>
-              <th className="px-5 py-2 mx-5 text-start">Genre</th>
-              <th className="px-5 py-2 mx-5 text-start">Stock</th>
-              <th className="px-5 py-2 mx-5 text-start">Rate</th>
+              <th className="px-5 py-2 mx-5 text-start border-b-1">Title</th>
+              <th className="px-5 py-2 mx-5 text-start border-b-1">Genre</th>
+              <th className="px-5 py-2 mx-5 text-start border-b-1">Stock</th>
+              <th className="px-5 py-2 mx-5 text-start border-b-1">Rate</th>
+              <th className="px-5 py-2 mx-5 text-start border-b-1"></th>
             </tr>
           </thead>
           <tbody>
             {movies.map(movie => (
               <tr key={movie._id} className='w-50px'>
-                <td className="px-5 py-2 mx-5 text-start">{movie.title}</td>
-                <td className="px-5 py-2 mx-5 text-start">{movie.genre.name}</td>
-                <td className="px-5 py-2 mx-5 text-start">{movie.numberInStock}</td>
-                <td className="px-5 py-2 mx-5 text-start">{movie.dailyRentalRate}</td>
+                <td className="px-5 py-2 mx-5 text-start border-b-1">{movie.title}</td>
+                <td className="px-5 py-2 mx-5 text-start border-b-1">{movie.genre.name}</td>
+                <td className="px-5 py-2 mx-5 text-start border-b-1">{movie.numberInStock}</td>
+                <td className="px-5 py-2 mx-5 text-start border-b-1">{movie.dailyRentalRate}</td>
+                <td className="px-5 py-2 mx-5 text-start border-b-1">
+                  <button onClick={() => this.handleDelete(movie)} className="w-30 h-8 bg-red-500 text-2xl text-white rounded-md">Delete</button>
+                </td>
               </tr>
             ))}
           </tbody>
