4665681fd8c97becdba3f19ee5da16bcf75e030c
Gurmesa <gurmesakedir96@gmail.com>
2025-04-19 10:11:25 +0300

    part-4 display table of movies menu and fix the frist error happend when i am create the fakeMovies.jsx

--- react-mosh/src/App.jsx
+++ react-mosh/src/App.jsx
@@ -6,8 +6,9 @@ function App() {
 
   return (
     <>
+       <Movies />
       <Counter />
-      <Movies />
+      
     </>
   )
 }
--- react-mosh/src/components/Movies.jsx
+++ react-mosh/src/components/Movies.jsx
@@ -1,35 +1,43 @@
 import React, { Component } from 'react';
-import getMovie from './fakeMovies';
+import {getMovies} from './fakeMovies';
 
 class Movies extends Component {
   state = { 
-    movies: getMovie()
-   } 
+    movies: getMovies()
+     // Initialize movies from fakeMovies.js
+  };
   render() { 
-    return <table className="table">
-      <thead>
-        <tr>
-          <th>Title</th>
-          <th>Genre</th>
-          <th>Stock</th>
-          <th>Rate</th>
-        </tr>
-      </thead>
-      <tbody>
-        {
-        this.state.movies.map(movie => (
-          <tr key={movie._id}>
-            <td>{movie.title}</td>
-            <td>{movie.genre.name}</td>
-            <td>{movie.numberInStock}</td>
-            <td>{movie.dailyRentalRate}</td>
-          </tr>
-        ))
-       }
-      </tbody>
-    </table>
+    const { movies } = this.state; // Destructure movies from state
+    console.log(this.state.movies);
 
+    if (movies.length === 0) return <p  className="text-2xl text-red-600 font-bold pl-2 mb-5 mt-10">There are no movies in the database.</p>;
+
+    return (
+      <div>
+        <p className="text-2xl text-red-600 font-bold pl-2 mb-5 mt-10">Showing {movies.length} movies in the database.</p>
+        <table className="table">
+          <thead>
+            <tr className='p-10px'>
+              <th className="px-5 py-2 mx-5 text-start">Title</th>
+              <th className="px-5 py-2 mx-5 text-start">Genre</th>
+              <th className="px-5 py-2 mx-5 text-start">Stock</th>
+              <th className="px-5 py-2 mx-5 text-start">Rate</th>
+            </tr>
+          </thead>
+          <tbody>
+            {movies.map(movie => (
+              <tr key={movie._id} className='w-50px'>
+                <td className="px-5 py-2 mx-5 text-start">{movie.title}</td>
+                <td className="px-5 py-2 mx-5 text-start">{movie.genre.name}</td>
+                <td className="px-5 py-2 mx-5 text-start">{movie.numberInStock}</td>
+                <td className="px-5 py-2 mx-5 text-start">{movie.dailyRentalRate}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        </div>
+    );
   }
 }
- 
+
 export default Movies;
