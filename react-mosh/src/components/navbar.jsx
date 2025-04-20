dff5dc679ecfec73a7ad7ba612d66c597da601bf
Gurmesa <gurmesakedir96@gmail.com>
2025-04-21 00:22:03 +0300

    create stateless functional component for navbar

--- react-mosh/src/components/navbar.jsx
+++ react-mosh/src/components/navbar.jsx
@@ -1,16 +1,13 @@
-import React, { Component } from 'react';
-class NavBar  extends Component {
-  render() { 
-    return (<div>
-      <na className=''>
-        <a herf="#" 
-        className='w-30 h-10 ml-10 mt-10 bg-gray-400 text-black text-2xl'>
-        NavBar{""}
-        <span className='mx-5  bg-gray-400 text-2xl text-black'>{this.props.totalCountres}</span>
-       </a>
-      </na>
-    </div>);
-  }
+import React from 'react';
+const NavBar = (props) => {
+  return (<div>
+    <na className=''>
+      <a herf="#" 
+      className='w-30 h-10 ml-10 mt-10 bg-gray-400 text-black text-2xl'>
+      NavBar{""}
+      <span className='mx-5  bg-gray-400 text-2xl text-black'>{props.totalCountres}</span>
+     </a>
+    </na>
+  </div>);
 }
- 
 export default NavBar;
